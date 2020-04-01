cfc9a986e4e90731ad389f8df545344bf8033aa1
Fernanda Retamal <fernanda.retamal@ideauno.cl>
2020-03-31 21:00:58 -0300

    fixes pequeños

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,25 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
     //libreria de animaciones
     AOS.init();
 
-    var loading = document.getElementById('loading');
-    var mensaje = document.getElementById('mensaje');
-
     function createCORSRequest(method, url) {
         var xhr = new XMLHttpRequest();
         if ("withCredentials" in xhr) {
             // Check if the XMLHttpRequest object has a "withCredentials" property.
-            // "withCredentials" only exists on XMLHTTPRequest2 objects.
             xhr.open(method, url, true);
         } else if (typeof XDomainRequest != "undefined") {
             // Otherwise, check if XDomainRequest.
-            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
             xhr = new XDomainRequest();
             xhr.open(method, url);
-
         } else {
             // Otherwise, CORS is not supported by the browser.
             xhr = null;
-
         }
         return xhr;
     }
@@ -29,7 +22,6 @@ document.addEventListener("DOMContentLoaded", function() {
     if (!xhr) {
         throw new Error('CORS not supported');
     }
-
 
     async function getPosts() 
     {
@@ -62,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
                         var div = document.createElement('div');  
                         div.innerHTML =`
                                 <div class="testimonie">
-                                    <img class="avatar" src="assets/images/person_${id}.jpg">
+                                    <img class="avatar" src="assets/images/person_${id}.jpg" alt="Testimony">
                                     <p class="comment">
                                         ${post.body}
                                     </p>
